4276a869f68409717f1b2902284b6341118b335b
unknown <roozbehghazavi@gmail.com>
2021-10-31 08:06:34 +0330

    cleaning directory

--- Saeed/parking-project/parkingowner/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from parkingowner.models import ParkingOwner
-
-# Register your models here.
-
-admin.site.register(ParkingOwner)
--- Saeed/parking-project/parkingowner/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path , include
-from .views import ParkingAV
-
-
-urlpatterns = [
-    path('addparking/', ParkingAV.as_view(), name='add-parking'),
-]
