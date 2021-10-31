a695cfffebe25d6eee568e468618ac52a3701e6a
Saeed Shahib Zadeh <saeedshahib@gmail.com>
2021-10-31 08:09:41 +0330

    mmd

--- Saeed/parking-project/carowner/models.py
+++ Saeed/parking-project/carowner/models.py
@@ -8,9 +8,9 @@ from users.models import CustomUser
 
 class CarOwner(models.Model):
 	user = models.OneToOneField(CustomUser,on_delete=models.CASCADE,null=True)
-	favoriteLocations = models.CharField(max_length=100)
-	profilePhoto = models.ImageField()
-	cash = models.IntegerField(default=0)
+	favoriteLocations = models.CharField(max_length=100,blank=True)
+	profilePhoto = models.ImageField(blank=True)
+	cash = models.IntegerField(default=0,blank=True)
 
 	def __str__(self):
 		return self.user.firstName + self.user.lastName
--- Saeed/parking-project/carowner/urls.py
+++ Saeed/parking-project/carowner/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path , include
 
+from carowner.views import CarOwnerCreate, CarOwnerDetail, CarOwnerUpdate, CarOwnerDelete, CarOwnerList
+
 
 urlpatterns = [
+    path('create/', CarOwnerCreate.as_view()),
+    path('', CarOwnerList.as_view()),
+    path('<int:pk>/', CarOwnerDetail.as_view()),
+    path('update/<int:pk>/', CarOwnerUpdate.as_view()),
+    path('delete/<int:pk>/', CarOwnerDelete.as_view()),
 ]
