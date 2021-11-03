4be95a6800e474d3db4f101a542ce104d90e9436
unknown <roozbehghazavi@gmail.com>
2021-11-03 18:03:45 +0330

    a

--- parking-project/parkingowner/admin.py
+++ parking-project/parkingowner/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from parkingowner.models import ParkingOwner
+from parkingowner.models import ParkingOwner,Parking
 
 # Register your models here.
 
 admin.site.register(ParkingOwner)
+admin.site.register(Parking)
