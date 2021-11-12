f66a84e7edaa2c3e45286b7392500de2c2675ad3
Saeed Shahib Zadeh <saeedshahib@gmail.com>
2021-11-12 21:19:15 +0330

    CRUD METHODS

--- parking-project/carowner/views.py
+++ parking-project/carowner/views.py
@@ -6,6 +6,7 @@ from rest_framework.response import Response
 from parkingowner.models import Parking
 from parkingowner.serializers import ParkingSerializer
 from .models import  Car, CarOwner
+from users.models import CustomUser
 from .pagination import CarOwnerPagination
 from rest_framework import generics, pagination, status
 from .serializers import CarOwnerSerializer, CarSerializer
@@ -13,7 +14,13 @@ import json
 import requests
 # Create your views here.
 
+
+
+
 ### Car Owner Views
+
+
+
 class CarOwnerCreate(generics.CreateAPIView):
     # API endpoint that allows creation of a new customer
     queryset = CarOwner.objects.all()
@@ -24,12 +31,18 @@ class CarOwnerList(generics.ListAPIView):
     queryset = CarOwner.objects.all()
     serializer_class = CarOwnerSerializer
 
+#Ruturns logged in CarOwner detail
 class CarOwnerDetail(generics.RetrieveAPIView):
     # API endpoint that returns a single customer by pk.
     queryset = CarOwner.objects.all()
     serializer_class = CarOwnerSerializer
 
+    def get(self, request, *args, **kwargs):
+        instance = get_object_or_404(CarOwner, user = request.user)
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
 
+#Update logged in CarOwner
 class CarOwnerUpdate(generics.RetrieveUpdateAPIView):
     # API endpoint that allows a customer record to be updated.
     queryset = CarOwner.objects.all()
@@ -37,7 +50,6 @@ class CarOwnerUpdate(generics.RetrieveUpdateAPIView):
 
     def update(self, request, *args, **kwargs):
         partial = kwargs.pop('partial', False)
-        print(request.user.id)
         instance = get_object_or_404(CarOwner, user = request.user)
         serializer = self.get_serializer(instance, data=request.data, partial=partial)
         serializer.is_valid(raise_exception=True)
@@ -51,17 +63,26 @@ class CarOwnerUpdate(generics.RetrieveUpdateAPIView):
         return Response(serializer.data)
     
 
+#Delete logged in CarOwner
 class CarOwnerDelete(generics.RetrieveDestroyAPIView):
     # API endpoint that allows a customer record to be deleted.
     queryset = CarOwner.objects.all()
     serializer_class = CarOwnerSerializer
 
+    def delete(self, request, *args, **kwargs):
+        get_object_or_404(CarOwner, user = request.user).delete()
+        user = self.request.user
+        user.delete()
+
+        return Response({"message" : "CarOwner deleted successfully"},status=status.HTTP_204_NO_CONTENT)
 
 
 
-### Car Views
+
+### Car Views 
 
 
+#Creates a car for the logged in CarOwner
 class CarCreate(generics.CreateAPIView):
     # API endpoint that allows creation of a new customer
     queryset = Car.objects.all()
@@ -77,7 +98,7 @@ class CarCreate(generics.CreateAPIView):
 
 
 
-
+#Shows List of all Cars Owned by the Logged in user
 class CarList(generics.ListAPIView):
     # API endpoint that allows customer to be viewed.
     queryset = Car.objects.all()
@@ -96,17 +117,41 @@ class CarList(generics.ListAPIView):
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
 
+#Returns Detail of a Car by id owned by the logged in CarOwner
 class CarDetail(generics.RetrieveAPIView):
     # API endpoint that returns a single customer by pk.
     queryset = Car.objects.all()
     serializer_class = CarSerializer
 
+    def get(self, request, *args, **kwargs):
+        owner = get_object_or_404(CarOwner, user = request.user)
+        instance = get_object_or_404(Car, id = request.data['id'], owner = owner)
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
+
+
+#Update a Car by id owned by the logged in CarOwner
 class CarUpdate(generics.RetrieveUpdateAPIView):
     # API endpoint that allows a customer record to be updated.
     queryset = Car.objects.all()
     serializer_class = CarSerializer
 
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        owner = get_object_or_404(CarOwner, user = request.user)
+        instance = get_object_or_404(Car, id = request.data['id'], owner = owner)
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
 
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data)
+
+#Delete a Car by id owned by the logged in CarOwner
 class CarDelete(generics.RetrieveDestroyAPIView):
     # API endpoint that allows a customer record to be deleted.
     queryset = Car.objects.all()
@@ -119,8 +164,13 @@ class CarDelete(generics.RetrieveDestroyAPIView):
         return Response({"message" : "Car deleted successfully"},status=status.HTTP_204_NO_CONTENT)
 
 
-# Parking
 
+
+# Parking views for CarOwner
+
+
+
+#Shows List of all parkings ordering by parking name for CarOwner
 class ParkingList(generics.ListAPIView):
     queryset = Parking.objects.all()
     serializer_class = ParkingSerializer
