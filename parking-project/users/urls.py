15bd6c69ebce76f95c8f2d57737525179bc2a63d
unknown <roozbehghazavi@gmail.com>
2021-11-14 10:55:05 +0330

    Show user info by token

--- parking-project/users/urls.py
+++ parking-project/users/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path , include
-from .views import CustomUserUpdate
+from .views import CustomUserUpdate,UserInfo
 
 urlpatterns = [
     path('rest-auth/', include('rest_auth.urls')),
     path('rest-auth/registration/', include('rest_auth.registration.urls')),
     path('account/', include('allauth.urls')),
-    path('update/', CustomUserUpdate.as_view())
+    path('update/', CustomUserUpdate.as_view()),
+    path('userinfo/', UserInfo.as_view()),
 
     # url(r'^accounts-rest/registration/account-confirm-email/(?P<key>.+)/$', confirm_email, name='account_confirm_email'),
 ]
--- parking-project/users/views.py
+++ parking-project/users/views.py
@@ -7,25 +7,35 @@ from rest_framework import generics, status
 from .serializers import CustomUserSerializer
 import json
 import requests
+from rest_auth.views import LoginView
 
 # Create your views here.
 
 
 class CustomUserUpdate(generics.RetrieveUpdateAPIView):
-    # API endpoint that allows a customer record to be updated.
-    queryset = CustomUser.objects.all()
-    serializer_class = CustomUserSerializer
+	# API endpoint that allows a customer record to be updated.
+	queryset = CustomUser.objects.all()
+	serializer_class = CustomUserSerializer
 
-    def update(self, request, *args, **kwargs):
-        partial = kwargs.pop('partial', False)
-        instance = get_object_or_404(CustomUser, id = request.user.id)
-        serializer = self.get_serializer(instance, data=request.data, partial=partial)
-        serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
+	def update(self, request, *args, **kwargs):
+		partial = kwargs.pop('partial', False)
+		instance = get_object_or_404(CustomUser, id = request.user.id)
+		serializer = self.get_serializer(instance, data=request.data, partial=partial)
+		serializer.is_valid(raise_exception=True)
+		self.perform_update(serializer)
 
-        if getattr(instance, '_prefetched_objects_cache', None):
-            # If 'prefetch_related' has been applied to a queryset, we need to
-            # forcibly invalidate the prefetch cache on the instance.
-            instance._prefetched_objects_cache = {}
+		if getattr(instance, '_prefetched_objects_cache', None):
+			# If 'prefetch_related' has been applied to a queryset, we need to
+			# forcibly invalidate the prefetch cache on the instance.
+			instance._prefetched_objects_cache = {}
 
-        return Response(serializer.data)
+		return Response(serializer.data)
+
+#Return user info by token
+class UserInfo(generics.RetrieveAPIView):
+	def get(self, request, *args, **kwargs):
+		user = get_object_or_404(CustomUser, auth_token = request.auth)
+		serializer=CustomUserSerializer(user)
+		return Response(serializer.data)
+		
+
