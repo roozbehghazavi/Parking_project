011321e3059c051edf353f8355a934b9cc2b471e
Saeed Shahib Zadeh <saeedshahib@gmail.com>
2021-11-22 22:08:46 +0330

    admin

--- parking-project/carowner/admin.py
+++ parking-project/carowner/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
-from carowner.models import Car, CarOwner
+from carowner.models import Car, CarOwner, Comment, Rate
 
 # Register your models here.
 
 
 admin.site.register(CarOwner)
 admin.site.register(Car)
+admin.site.register(Rate)
+admin.site.register(Comment)
