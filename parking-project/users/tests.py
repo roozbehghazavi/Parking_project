87dc45984676bc9e769c0405c7150b1a6dea744d
roozbehghazavi <roozbehghazavi3@gmail.com>
2022-03-23 17:25:43 +0430

    Register & Login Test

--- parking-project/users/tests.py
+++ parking-project/users/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
-
+from django.urls import reverse
+from rest_framework.test import APIClient    
+from rest_framework.test import APITestCase
+import users
+from users.models import CustomUser
 # Create your tests here.
+
+
+class RegistrationTest(APITestCase):
+
+    def test(self):  
+        self.client = APIClient()
+        response = self.client.post('/users/rest-auth/registration/', {'email':'mmdbfrst@gmail.com','password1':'affarin234','password2':'affarin234','role':'P'})
+        assert response.status_code == 201
+
+
+
+class CheckUserViewTest(APITestCase):
+
+    def test_check_user(self):
+        self.client = APIClient()
+        user = CustomUser.objects.create_user('username', 'Pas$w0rd')
+        self.assertTrue(self.client.login(username='username', password='Pas$w0rd'))
