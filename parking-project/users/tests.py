5465c1abf5dac6b086552ec4508833a53bdbda60
Saeed Shahib Zadeh <saeedshahib@gmail.com>
2022-03-25 19:52:04 +0430

    edit profile test added

--- parking-project/users/tests.py
+++ parking-project/users/tests.py
@@ -1,3 +1,4 @@
+from http import client
 from django.test import TestCase
 from django.urls import reverse
 from rest_framework.test import APIClient    
@@ -22,3 +23,22 @@ class LoginTest(APITestCase):
         self.client = APIClient()
         self.client.post('/users/rest-auth/registration/', {'email':'mmdbfrst@gmail.com','password1':'affarin234','password2':'affarin234','role':'P'})
         self.assertTrue(self.client.login(email='mmdbfrst@gmail.com',password='affarin234'))
+        
+
+
+class EditProfileTest(APITestCase):
+
+    def test_edit_profile(self):
+        self.client = APIClient()
+        response = self.client.post('/users/rest-auth/registration/', {'email':'mmdbfrst@gmail.com','password1':'affarin234','password2':'affarin234','role':'P'})
+        self.assertEqual(response.status_code, 201)
+        token = response.data.get('key')
+
+        self.client.credentials(HTTP_AUTHORIZATION = 'token ' + token)
+
+        url = reverse('edit-profile')
+
+        self.client.put(url, {'firstName' : 'mmd', 'lastName':'bakhfazian'},format='json')
+
+        self.assertEqual(CustomUser.objects.first().firstName, 'mmd')
+        self.assertEqual(CustomUser.objects.first().lastName, 'bakhfazian')
--- parking-project/users/urls.py
+++ parking-project/users/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path , include
-from .views import CustomUserUpdate,UserRole,CustomLoginView,FacebookLogin,GoogleLogin
+from .views import CustomUserUpdate,UserRole,CustomLoginView
 
 urlpatterns = [
     path('rest-auth/', include('rest_auth.urls')),
-    path('rest-auth/registration/', include('rest_auth.registration.urls')),
+    path('rest-auth/registration/', include('rest_auth.registration.urls'), name="register"),
     path('account/', include('allauth.urls')),
-    path('update/', CustomUserUpdate.as_view()),
+    path('update/', CustomUserUpdate.as_view(), name='edit-profile'),
     path('userrole/', UserRole.as_view()),
     path('custom/login/', CustomLoginView.as_view()),
-    path('rest-auth/facebook/', FacebookLogin.as_view(), name='fb_login'),
-    path('rest-auth/google/', GoogleLogin.as_view(), name='google_login'),
+
+    # url(r'^accounts-rest/registration/account-confirm-email/(?P<key>.+)/$', confirm_email, name='account_confirm_email'),
 ]
