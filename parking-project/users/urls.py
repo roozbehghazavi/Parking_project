4b8d24bb9654346cccf3646b80b7444c812e7db0
roozbehghazavi <roozbehghazavi3@gmail.com>
2022-03-25 20:40:50 +0430

    Urls

--- parking-project/users/urls.py
+++ parking-project/users/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path , include
-from .views import CustomUserUpdate,UserRole,CustomLoginView
+from .views import CustomUserUpdate,UserRole,CustomLoginView,GoogleLogin,FacebookLogin
 
 urlpatterns = [
     path('rest-auth/', include('rest_auth.urls')),
     path('rest-auth/registration/', include('rest_auth.registration.urls'), name="register"),
+    path('rest-auth/facebook/', FacebookLogin.as_view(), name='fb_login'),
+    path('rest-auth/google/', GoogleLogin.as_view(), name='google_login'),
     path('account/', include('allauth.urls')),
     path('update/', CustomUserUpdate.as_view(), name='edit-profile'),
     path('userrole/', UserRole.as_view()),
     path('custom/login/', CustomLoginView.as_view()),
-
-    # url(r'^accounts-rest/registration/account-confirm-email/(?P<key>.+)/$', confirm_email, name='account_confirm_email'),
 ]
