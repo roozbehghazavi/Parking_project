895e21de389b33c8bdb3f70f4306a2a55247c6ab
Saeed Shahib Zadeh <saeedshahib@gmail.com>
2022-03-25 20:10:21 +0430

    more prints

--- parking-project/users/tests.py
+++ parking-project/users/tests.py
@@ -31,7 +31,9 @@ class EditProfileTest(APITestCase):
     def test_edit_profile(self):
         self.client = APIClient()
         response = self.client.post('/users/rest-auth/registration/', {'email':'mmdbfrst@gmail.com','password1':'affarin234','password2':'affarin234','role':'P'})
-        print(response)
+        print(response.data)
+        print(response.text)
+        print(response.json)
         self.assertEqual(response.status_code, 201)
         token = response.data.get('key')
         print(token)
