4e2a6f9b915a95c473e3680cfb18157c9a24eb38
roozbehghazavi <roozbehghazavi3@gmail.com>
2022-04-22 12:32:46 +0430

    Phonenumber Test

--- parking-project/users/tests.py
+++ parking-project/users/tests.py
@@ -1,4 +1,5 @@
 from http import client
+from urllib import response
 from django.test import TestCase
 from django.urls import reverse
 from rest_framework.test import APIClient    
@@ -43,3 +44,23 @@ class EditProfileTest(APITestCase):
 
         self.assertEqual(CustomUser.objects.first().firstName, 'mmd')
         self.assertEqual(CustomUser.objects.first().lastName, 'bakhfazian')
+    
+class PhonenumberOtpTest(APITestCase):
+    def test_phonenumber_otp(self):
+        self.client=APIClient()
+        
+        #register User
+        register = self.client.post('/users/rest-auth/registration/', {'email':'mmdbfrst@gmail.com','password1':'affarin234','password2':'affarin234','role':'P','phoneNumber':'09123456789'})
+        self.assertEqual(register.status_code, 201)
+
+        #Get credentials
+        token = Token.objects.first().key
+        self.client.credentials(HTTP_AUTHORIZATION = 'token ' + token)
+
+        #Put phoneNumber
+        self.client.put('users/edit-profile', {'phoneNumber' : '09123456789'})
+        self.assertEqual(CustomUser.objects.first().phoneNumber, '09123456789')
+
+        #OTP test
+        response = self.client.post('/users/otpvalidation/',{'phoneNumber':'09123456789'})
+        self.assertEqual(response.status_code, 200)
